9573a97988769664dc8da5dd2863ecfb1fa43eb5
Art Vardanyan <akvardanyan99@gmail.com>
2023-10-20 10:10:24 -0400

    final submit

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -3,18 +3,13 @@ import { useContext } from 'react';
 import CartContext from '../contexts/CartContext';
 
 const Item = props => {
-
 	const {alterCart} = useContext(CartContext);
-
 	const handleClick = item => {
 		alterCart(item, 'remove');
 	}
-
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
-
-
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
